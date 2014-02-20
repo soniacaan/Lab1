0380b340b4d778d7acc1d0481f24b2b13c9955cd
sonia <sonia.khan@gmail.com>
2014-02-20 00:11:06 +0000

    node.js lab

---
+++ lab4/routes/project.js
@@ -0,0 +1,11 @@
+//project controller file
+
+exports.viewProject = function(req, res){
+	//controller code goes here
+	var name = req.params.name;
+  	console.log("The project name is: " + name);
+	res.render('project', {
+		'projectName': name
+
+});
+};
